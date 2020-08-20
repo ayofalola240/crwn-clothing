6ab7b985553959b7d3943c14de0a643df5b064cd
Falola Ibrahim A <ayofalola240@gmail.com>
2020-08-20 12:38:20 +0100

    Adding stripe payment integration to our application

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51HIBTYJbl3aKKYYIlIAp5OPTYD67fBFgNoA6iR1zOaeA857xoGcI0Bin4jwH5PVrVcrri3R65hSJw7dmZbYDiAc0007ak86jTZ';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
